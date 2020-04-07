5a8e79373541d234eea96b9ab6f9b1cff3d930f3
Daniela Estrada <d.anielaestradam2013@gmail.com>
2020-04-06 23:18:17 -0500

    first steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tour-heroes-app';
+  public title: string;
+
+  constructor() {
+    this.title = 'Tour of Heroes';
+  }
 }
